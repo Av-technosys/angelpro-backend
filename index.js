28a739d6a7b46ec3aecb35f9832e8a9aeeca7a47
Av-technosys <Avtechnosys02@gmail.com>
2025-05-24 14:37:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     credentials: true,
     // origin: "http://localhost:5173",
-    origin: "https://chimerical-starlight-6eb981.netlify.app/"
+    origin: "https://chimerical-starlight-6eb981.netlify.app"
   })
 );
 
